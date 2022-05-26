a68f13b9e2b144cc8d173868cd12ef4096c07bd8
Linh18110144 <Linh18110144@users.noreply.github.com>
2022-05-26 17:14:59 +0700

    Product Detail

---
+++ TTNM/js/productDetail.js
@@ -0,0 +1,13 @@
+function addToCart(){
+    alert("The product has been added to cart. ");
+}
+
+function chooseImg1(){
+    // let value = document.getElementById('img1').src.value;
+    document.getElementById('bigImg').src='/images/product-1.png';
+}
+
+function chooseImg2(){
+    // let value = document.getElementById('img1').src.value;
+    document.getElementById('bigImg').src='/images/product-2.png';
+}
